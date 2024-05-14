6ea211b20b30ca9be6b5e30dcca35312f08afe9f
freelytian <osk9ehdms01@gmail.com>
2024-05-14 17:56:06 -0300

    nova estilização

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -32,14 +32,17 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <div className="rounded-md border">
+    <div className="rounded-md border overflow-hidden min-w-[900px]">
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
                 return (
-                  <TableHead key={header.id}>
+                  <TableHead
+                    key={header.id}
+                    className="text-center bg-black text-white"
+                  >
                     {header.isPlaceholder
                       ? null
                       : flexRender(
@@ -60,7 +63,7 @@ export function DataTable<TData, TValue>({
                 data-state={row.getIsSelected() && "selected"}
               >
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
+                  <TableCell key={cell.id} className="text-center">
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
