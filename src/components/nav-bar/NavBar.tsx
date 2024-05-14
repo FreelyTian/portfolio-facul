b335f8c10be3039a323fa4eeaa641fda0a548acd
freelytian <osk9ehdms01@gmail.com>
2024-05-14 17:58:14 -0300

    redirect atualizado com nova rota

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -5,7 +5,7 @@ import { signIn } from "@/auth.ts";
 
 export function NavBar() {
   return (
-    <div className="flex h-16 w-full items-center flex-row justify-between px-4 md:px-6">
+    <div className="flex h-10 border w-full items-center flex-row justify-between px-4 md:px-6">
       <Link className="flex items-center gap-2 text-lg font-semibold" href="#">
         <PowerOffIcon size={24} />
         <span>Myself</span>
@@ -28,10 +28,13 @@ export function NavBar() {
         <form
           action={async () => {
             "use server";
-            await signIn("google", { redirectTo: "/admin" });
+            await signIn("google", { redirectTo: "/admin/Posts" });
           }}
         >
-          <Button type="submit" className="rounded-md shadow-sm p-2 border hover:bg-black hover:text-white">
+          <Button
+            type="submit"
+            className="rounded-md shadow-sm p-2 border hover:bg-black hover:text-white"
+          >
             Sign in
           </Button>
         </form>
