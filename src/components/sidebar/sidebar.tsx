d69376e40193bbf3832a1a8cfdca401e84732d51
freelytian <osk9ehdms01@gmail.com>
2024-05-15 18:42:32 -0300

    Adiciona novo menu de interações com a tabela

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -6,8 +6,10 @@ import Link from "next/link";
 import {
   BookAIcon,
   BookIcon,
+  Edit,
   FileIcon,
   FileSpreadsheetIcon,
+  Plus,
 } from "lucide-react";
 import {
   Tooltip,
@@ -15,6 +17,17 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "../ui/tooltip";
+import {
+  DropdownMenu,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@radix-ui/react-dropdown-menu";
 
 export default function SideBar() {
   return (
@@ -50,6 +63,50 @@ export default function SideBar() {
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Link
+            href="#"
+            className="bg-white flex flex-row justify-center rounded-md h-12 text-center hover:bg-black hover:text-white border-stone-300 shadow-md shadow-zinc-300"
+          >
+            <Edit className="mt-3" />
+          </Link>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          align="center"
+          sideOffset={6}
+          side="right"
+          className="bg-white mt-11 duration-300 animate-in slide-in-from-left-2 fade-in-15 zoom-in-95 rounded-md overflow-hidden border-zinc-200 border w-36"
+        >
+          <DropdownMenuLabel className="bg-black text-white w-full">
+            {" "}
+            Ações
+          </DropdownMenuLabel>
+          <DropdownMenuGroup className="flex flex-col gap-1 p-1">
+            <DropdownMenuLabel>Editar</DropdownMenuLabel>
+            <DropdownMenuItem className="flex flex-row gap-1">
+              <Edit></Edit>
+              <Link href={"#"}>Posts</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-row gap-1">
+              <Edit></Edit>
+              <Link href={"#"}>Matérias</Link>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator></DropdownMenuSeparator>
+          <DropdownMenuGroup className="flex flex-col gap-1 p-1">
+            <DropdownMenuLabel>Criar</DropdownMenuLabel>
+            <DropdownMenuItem className="flex flex-row gap-1">
+              <Plus></Plus>
+              <Link href={"#"}>Posts</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-row gap-1">
+              <Plus></Plus>
+              <Link href={"#"}>Matérias</Link>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
