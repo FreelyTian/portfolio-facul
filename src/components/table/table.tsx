791bd7f0362abf15996f51aa4e27a60392719931
freelytian <osk9ehdms01@gmail.com>
2024-08-14 17:44:06 -0300

    fix: refatorada novo instaciador

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { DataTable } from "../ui/data-table";
 
-export function MyTable(props: { texto: any; columns: any; data: any }) {
+export function MyTable(props: { columns: any; data: any }) {
   return (
-    <div className="flex gap-2 flex-col justify-start ml-3.5 p-2 -z-10">
-      <h1 className="text-4xl">{props.texto}</h1>
-      <DataTable columns={props.columns} data={props.data} />
+    <div>
+      <DataTable columns={props.columns} data={props.data}></DataTable>
     </div>
   );
 }
