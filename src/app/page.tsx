d6f857608d6819b7bea7181b5a22bd7c8998de8c
freelytian <osk9ehdms01@gmail.com>
2024-08-14 17:38:41 -0300

    fix: padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { NavBar } from "@/components/nav-bar/NavBar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between w-full">
+    <div className="flex min-h-screen flex-col items-center justify-between w-full p-2">
       <NavBar />
     </div>
   );
