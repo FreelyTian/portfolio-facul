05be9348fcd49d30ace220cdb8456802149eeed6
freelytian <osk9ehdms01@gmail.com>
2024-08-14 17:42:45 -0300

    remove: menu de conexto ficou obsoleto e foi removido

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -63,50 +63,7 @@ export default function SideBar() {
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Link
-            href="#"
-            className="bg-white flex flex-row justify-center rounded-md h-12 text-center hover:bg-black hover:text-white border-stone-300 shadow-md shadow-zinc-300"
-          >
-            <Edit className="mt-3" />
-          </Link>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent
-          align="center"
-          sideOffset={6}
-          side="right"
-          className="bg-white mt-11 duration-300 animate-in slide-in-from-left-2 fade-in-15 zoom-in-95 rounded-md overflow-hidden border-zinc-200 border w-36"
-        >
-          <DropdownMenuLabel className="bg-black text-white w-full">
-            {" "}
-            Ações
-          </DropdownMenuLabel>
-          <DropdownMenuGroup className="flex flex-col gap-1 p-1">
-            <DropdownMenuLabel>Editar</DropdownMenuLabel>
-            <DropdownMenuItem className="flex flex-row gap-1">
-              <Edit></Edit>
-              <Link href={"#"}>Posts</Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem className="flex flex-row gap-1">
-              <Edit></Edit>
-              <Link href={"#"}>Matérias</Link>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator></DropdownMenuSeparator>
-          <DropdownMenuGroup className="flex flex-col gap-1 p-1">
-            <DropdownMenuLabel>Criar</DropdownMenuLabel>
-            <DropdownMenuItem className="flex flex-row gap-1">
-              <Plus></Plus>
-              <Link href={"#"}>Posts</Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem className="flex flex-row gap-1">
-              <Plus></Plus>
-              <Link href={"#"}>Matérias</Link>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      
     </div>
   );
 }
