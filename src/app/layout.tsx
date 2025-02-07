4aa40663f34e157598559da388294565d55c8459
FreelyTian <osk9ehdms01@gmail.com>
2025-02-07 15:51:59 -0300

    fix: alterar idioma da aplicação para português

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Provider } from "jotai";
+import { NavBar } from "@/components/nav-bar/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-        <body className={inter.className}>{children}</body>
+    <html lang="pt-Br">
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
